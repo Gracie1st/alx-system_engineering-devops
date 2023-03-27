9be0c4d7fb3bcf7c5928634e002e4150ae78c3ad
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2023-03-28 00:09:30 +0100

    Tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,10 @@
-#!/usr/bin/env bash
-# Create a new ssh key (type=rsa, bits=4096, name=school, passphrase=betty)
-ssh-keygen -t rsa -b 4096 -f school -N betty
+# Set the config of a ssh_file
+file_line { 'Change the main private key':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+}
+
+file_line { 'No Authenticate with passowrd':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
